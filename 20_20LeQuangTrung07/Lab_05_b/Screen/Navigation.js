14bd36cfe737c38cdafd484db35550caddae8da8
Le-Quang-Trung <leqtrungggg3105@gmail.com>
2023-10-26 13:58:05 +0700

    sửa Navigation.js

--- 20_20LeQuangTrung07/Lab_05_b/Screen/Navigation.js
+++ 20_20LeQuangTrung07/Lab_05_b/Screen/Navigation.js
@@ -1,30 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet, Text, View } from "react-native";
 import Screen01 from "./Screen01";
 import Screen02 from "./Screen02";
 import Screen03 from "./Screen03";
+import React from "react";
 
 const Stack = createNativeStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Screen01">
-        <Stack.Screen
-          name="Screen01"
-          component={Screen01}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="Screen02"
-          component={Screen02}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
-        <Stack.Screen
-          name="Screen03"
-          component={Screen03}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+        <Stack.Screen name="Screen01" component={Screen01} options={{ headerShown: false }}/>
+        <Stack.Screen name="Screen02" component={Screen02} options={{ headerShown: false }}/>
+        <Stack.Screen name="Screen03" component={Screen03} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
